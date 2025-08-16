bd9bde0eeffe658ce21ce0bea92cbacede44a8a7
Bruno Martins <bgcngm@gmail.com>
2025-08-16 23:00:09 +0100

    qcom: Remove cryptfshw remnants
    Change-Id: Ia6e19cd2d8701e133881cb2f70a0756595dd36a2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -26,7 +26,6 @@ ifeq (,$(filter msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_B
 BOARD_SEPOLICY_M4DEFS += \
     display_vendor_data_file=vendor_display_vendor_data_file \
     hal_gnss_qti=vendor_hal_gnss_qti \
-    hal_keymaster_qti_exec=vendor_hal_keymaster_qti_exec \
     hal_perf_default=vendor_hal_perf_default \
     location_domain=vendor_location \
     persist_block_device=vendor_persist_block_device \
